70049aadf750f6b944611ca04dc7cd9ffdb1abc6
Priyanka singh <160533231+PRIYANKA22BSC@users.noreply.github.com>
2025-04-06 09:43:00 +0530

    commited

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/auth-router.js';
-import contactrouter from './routes/contact-router.js';
-import servicerouter from './routes/service-router.js';
-import adminrouter from './routes/admin-router.js';
-import connectdb from './config/database.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import router from "./routes/auth-router.js";
+import contactrouter from "./routes/contact-router.js";
+import servicerouter from "./routes/service-router.js";
+import adminrouter from "./routes/admin-router.js";
+import connectdb from "./config/database.js";
+import dotenv from "dotenv";
 
+const app = express();
 dotenv.config();
-const app=express();
-const corsOptions={
-    origin:"https://manage-hub-sepia.vercel.app",
-    methods:"GET,POST,UPDATE,DELETE,PATCH,HEAD",
-    credentials:true
-}
+const corsOptions = {
+  origin: "https://manage-hub-sepia.vercel.app",
+  methods: "GET,POST,UPDATE,DELETE,PATCH,HEAD",
+  credentials: true,
+};
+connectdb();
 app.use(cors(corsOptions));
-app.use(express.urlencoded({extended:false}));
-app.use(express.json()); 
-app.use('/api',[router,contactrouter,servicerouter]);      //because app.use() expects a single middleware function or an array of middleware functions as its second argument.
-app.use('/api/admin',adminrouter);
-connectdb()
-.then(app.listen(8000,()=>{console.log("Server has been started.")}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/api", [router, contactrouter, servicerouter]); //because app.use() expects a single middleware function or an array of middleware functions as its second argument.
+app.use("/api/admin", adminrouter);
 
+app.listen(8000, () => {
+  console.log("Server has been started.");
+});
